9a02fee9b0a4988e2c5b85bf9c40f7f582c5123e
Arfat Fakih <arfat.fakih@somaiya.edu>
2024-08-12 12:51:35 +0530

    new example is solved and one new file is added

---
+++ FifthQue.java
@@ -0,0 +1,36 @@
+public class FifthQue {
+    public static void main(String[] args) {
+        System.out.println("Diamond Pattern: ");
+        int n = 4;
+
+        //Upper half
+        for(int i=1; i<=n; i++){
+            //spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            //Stars
+            for(int j=1; j<=2*i-1; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        //lower half
+        for(int i=n; i>=1; i--){
+            //spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            //Stars
+            for(int j=1; j<=2*i-1; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
