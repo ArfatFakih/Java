f499d1bb32bcd973453b2467387d4f048cadcc6b
Arfat Fakih <arfat.fakih@somaiya.edu>
2024-10-14 17:50:45 +0530

    Another method of Reverse Number

--- ReverseOfNumber.java
+++ ReverseOfNumber.java
@@ -2,10 +2,19 @@ public class ReverseOfNumber {
     public static void main(String[] args) {
         int n = 10899;
         
+            // while(n > 0){
+            //     int lastDigit = n % 10;
+            //     System.out.print(lastDigit + " ");
+            //     n = n / 10; // n /= 10 
+            // } 
+
+        //Another method
+        int rev = 0;
         while(n > 0){
             int lastDigit = n % 10;
-            System.out.print(lastDigit + " ");
-            n = n / 10; // n /= 10 
-        } 
+            rev = (rev * 10) + lastDigit;
+            n /= 10;
+        }
+        System.out.println(rev);
     }
 }
