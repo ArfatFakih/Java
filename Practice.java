23abb79c10ef77fe62e7bedc3e8d5e7eddcb8dd9
Arfat Fakih <arfatfakih30@gmail.com>
2025-01-17 12:24:35 +0530

    Some Bitwise problems are done

--- Practice.java
+++ Practice.java
@@ -3,37 +3,28 @@
 
 public class Practice {
 
-    public static int[] arrays(int numbers[]){
-        int n = numbers.length;
-
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
-
-        prefix[0] = numbers[0];
-        for(int i=1; i<n; i++){
-            prefix[i] = prefix[i-1] * numbers[i];
+    public static String toUpperCasee(String str){
+        String newStr = "";
+        
+        for(int i=0; i<str.length(); i++){
+            Integer count = 1;
+            while(i<str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newStr += str.charAt(i);
+            if(count > 1){
+                newStr += count.toString();
+            }
         }
 
-        suffix[n-1] = numbers[n-1];
-        for(int i=n-2; i>=0; i--){
-            suffix[i] = suffix[i+1] * numbers[i];
-        }
-
-        int[] ans = new int[n];
-
-        ans[0] = suffix[1];
-        ans[n-1] = prefix[n-2];
-        for(int i=1; i<=n-2; i++){
-            ans[i] = prefix[i-1] * suffix[i+1];
-        }
-
-        return ans;
+        return newStr;
     }
 
 
     public static void main(String[] args) {
-        // int arr[] = {2, 4, 6, 8, 10, 12, 14, 16};
-        int numbers[] = {-1, 1, 0, -3, 3};
-        arrays(numbers);    
+        String str = "aaabbcccdd";
+        System.out.print(toUpperCasee(str));
+
     }
 }
