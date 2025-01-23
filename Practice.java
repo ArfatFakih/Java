d81e5247f0a327de2acef3adbcb4ac42cefa4107
Arfat Fakih <devilsevil92@gmail.com>
2025-01-23 10:53:58 +0530

    Merge Sort code is done

--- Practice.java
+++ Practice.java
@@ -3,23 +3,18 @@
 
 public class Practice {
 
-    public static int factorail(int arr[], int key, int i){
-
-        if(i == arr.length){
-            return -1;
+    public static void decThenInc(int n){
+        if(n == 1){
+            System.out.println(n);
+            return;
         }
-        int isFound = factorail(arr, key, i+1);
-
-        if(isFound == -1 && arr[i] == key){
-            return i;
-        }
-
-        return isFound;
+        System.out.println(n);
+        decThenInc(n-1);
+        System.out.println(n);
     }
 
 
     public static void main(String[] args) {
-        int arr[] = {1, 2, 6, 8, 5, 3, 9, 5, 4};
-        System.out.println(factorail(arr, 5, 0));
+        decThenInc(5);
     }
 }
