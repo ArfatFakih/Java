7dda504c02d45b0aa8f798e19868c9604c3cc045
Arfat Fakih <devilsevil92@gmail.com>
2025-06-02 20:06:10 +0530

    Pair sum problem is solved

---
+++ ArrayLists/PairSum2.java
@@ -0,0 +1,53 @@
+//Rotated and Sorted array is given 
+
+package ArrayLists;
+
+import java.util.ArrayList;
+
+public class PairSum2 {
+
+    // Time complexity - O(n)
+    public static boolean pairSum2(ArrayList<Integer> list, int target){
+        int bp = -1;
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i) > list.get(i+1)) { //breaking point
+                bp = i;
+                break;
+            }
+        }
+
+        int n = list.size();
+        int left = bp + 1; //smallest
+        int right = bp;  //largest
+
+        while(left != right){
+            if(list.get(left) + list.get(right) == target){
+                return true;
+            }
+
+            if(list.get(left) + list.get(right) < target){
+                left = (left + 1) % n;
+            }else {
+                right = (n + right - 1) % n;
+            }
+        }
+
+        return false;
+        
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+
+        int target = 16;
+
+        System.out.println(pairSum2(list, target));
+    }
+}
