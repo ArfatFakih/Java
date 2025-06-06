ff5f654cec330c822316fea24fabea4822b10271
Arfat Fakih <devilsevil92@gmail.com>
2025-06-06 14:39:40 +0530

    Remove cycle from Linked List code

--- LinkedLists/LinkedList.java
+++ LinkedLists/LinkedList.java
@@ -210,6 +210,36 @@ public class LinkedList {
         return false; //cycle or loop doesn't exists
     }
 
+    public static void removeCycle(){
+        //Detect Cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                cycle = true;
+                break;
+            }
+        }
+        if(cycle == false){
+            return;
+        }
+
+        //find meeting point 
+        slow = head;
+        Node prev = null;
+        while(slow != fast){
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        //remove cycle -> last.next = null
+        prev.next = null;
+    }
+
     public void print(){ //O(n)
         Node temp = head;
         if(head == null){
@@ -253,12 +283,26 @@ public class LinkedList {
         // System.out.println(ll.checkPalindrome());
 
         //Data to check is cycle or loop is there or not in Linked List
+        // head = new Node(1);
+        // head.next = new Node(2);
+        // head.next.next = new Node(3);
+        // head.next.next.next = head;
+        // // 1->2->3->1
+        // System.out.println(isCycle());
+        
+
+        //Data to remove cycle from Linked list code
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next = new Node(3);
-        head.next.next.next = head;
-        // 1->2->3->1
+        head.next.next.next = temp;
+        // 1->2->3->2
+
         System.out.println(isCycle());
+        removeCycle();
+        System.out.println(isCycle());
+
 
     }
 }
