d9164b9232f34f3cf9eae6411e17d9b4f6cc18f6
Arfat Fakih <devilsevil92@gmail.com>
2025-06-18 11:22:24 +0530

    Implement Stack using two queue code

--- QueueDSA/QueueUsingTwoStack.java
+++ QueueDSA/QueueUsingTwoStack.java
@@ -44,14 +44,14 @@ public class QueueUsingTwoStack {
     }
 
     public static void main(String[] args) {
-        Queue q = new Queue();
-        q.add(1);
-        q.add(2);
-        q.add(3);
+        // Queue q = new Queue();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
 
-        while(!q.isEmpty()){
-            System.out.println(q.peek());
-            q.remove();
-        }
+        // while(!q.isEmpty()){
+        //     System.out.println(q.peek());
+        //     q.remove();
+        // }
     }
 }
