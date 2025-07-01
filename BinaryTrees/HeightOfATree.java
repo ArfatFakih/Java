c04bb5ae6f0ec9e49edb76fd7d9ea56f13832c59
Arfat Fakih <devilsevil92@gmail.com>
2025-07-01 12:05:06 +0530

    Count total Number of Nodes in a tree

--- BinaryTrees/HeightOfATree.java
+++ BinaryTrees/HeightOfATree.java
@@ -24,6 +24,21 @@ public class HeightOfATree {
         return Math.max(lh, rh) + 1;
     }
 
+
+    //Count the Number of Nodes in tree
+
+    public static int count(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftCount = count(root.left);
+        int rightCount = count(root.right);
+
+        return leftCount + rightCount + 1;
+    }
+
+
     public static void main(String[] args) {
         /* 
                     1
@@ -41,6 +56,9 @@ public class HeightOfATree {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        System.out.println(heightOfTree(root));
+        // System.out.println(heightOfTree(root));
+
+        System.out.println(count(root));
+        
     }
 }
