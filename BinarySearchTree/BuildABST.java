bc97daa2fde08e6431b4035b330a82af9dd3a31d
Arfat Fakih <devilsevil92@gmail.com>
2025-07-10 16:26:59 +0530

    Delete in BST

--- BinarySearchTree/BuildABST.java
+++ BinarySearchTree/BuildABST.java
@@ -54,9 +54,64 @@ public class BuildABST {
             return search(root.right, key);
         }
     }
+
+    //Delete
+
+    public static Node delete(Node root, int val){
+        if(root.data < val){
+            root.right = delete(root.right, val);
+        }else if(root.data > val){
+            root.left = delete(root.left, val);
+        }else {
+            //case - 1 - leaf node
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            //case - 2 - single child
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+
+            //case - 3 - both children
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
  
     public static void main(String[] args) {
-        int values[] = {5, 1, 3, 4, 2, 7};
+        // int values[] = {5, 1, 3, 4, 2, 7};
+        // Node root = null;
+
+        // for(int i=0; i<values.length; i++){
+        //     root = insert(root, values[i]);
+        // }
+
+        // inorder(root);
+        // System.out.println();
+
+
+        // if(search(root, 5)){
+        //     System.out.println("Found");
+        // }else{
+        //     System.out.println("Not Found");
+        // }
+
+        
+        //Delete data
+
+        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
 
         for(int i=0; i<values.length; i++){
@@ -66,11 +121,9 @@ public class BuildABST {
         inorder(root);
         System.out.println();
 
+        root = delete(root, 5);
+        System.out.println();
         
-        if(search(root, 5)){
-            System.out.println("Found");
-        }else{
-            System.out.println("Not Found");
-        }
+        inorder(root);
     }   
 }
