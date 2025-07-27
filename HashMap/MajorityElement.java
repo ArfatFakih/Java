7108cbb92bc175fc04d99ea38ade5ecc286f26bd
Arfat Fakih <devilsevil92@gmail.com>
2025-07-27 07:53:30 +0530

    Majority Element and Valid Anagram problem using Hashmap

---
+++ HashMap/MajorityElement.java
@@ -0,0 +1,21 @@
+package HashMap;
+
+import java.util.*;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 2, 5, 1, 3, 1, 5, 1};
+        int n = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for(Integer key : map.keySet()){
+            if(map.get(key) > n/3){
+                System.out.println(key);
+            }
+        }
+    }
+}
